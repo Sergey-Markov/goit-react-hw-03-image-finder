46e80d5e1ee8a5c7f34f20e39971890825289602
Sergey-Markov <1nativefl@gmail.com>
2021-09-01 15:40:42 +0300

    add first components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Searchbar from './Components/Searchbar/Searchbar';
+import ImageGallery from './Components/ImageGallery/ImageGallery';
 
 function App() {
-  return <div className="App">my app</div>;
+  return (
+    <div className="App">
+      <Searchbar />
+      <ImageGallery />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Searchbar/Searchbar.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export default class Searchbar extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <p>Searchbar</p>
+      </div>
+    );
+  }
+}
